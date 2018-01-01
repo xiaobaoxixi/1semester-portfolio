520b7d9987611d20a4c9c8e4cd6f73c283d526fc
Yan Li <Yan@Yans-MacBook-Air.local>
2018-01-01 08:18:12 +0100

    video play progress bar

--- js/video.js
+++ js/video.js
@@ -1,13 +1,35 @@
 let videoStoryboard = document.querySelector('img.storyboard');
-let defaultImgNr = 1;
-
+let defaultImgNr = 0;
+let progressLine = document.querySelector('.progressLine');
+let videos = document.querySelectorAll('video');
+let pause = false;
+// switch between storyboard imgs
 videoStoryboard.addEventListener('click', changeImg);
 function changeImg(){
-    if(defaultImgNr !== 7){
+    if(defaultImgNr !==7){
         defaultImgNr++;
-        videoStoryboard.style.backgroundImage = "url('img/video/storyboard"+defaultImgNr+".png')";
+//        videoStoryboard.style.backgroundImage = "url('img/video/storyboard"+defaultImgNr+".png')";
     } else {
         defaultImgNr = 1;
-        videoStoryboard.style.backgroundImage = "url('img/video/storyboard"+defaultImgNr+".png')";
     }
-};
+    videoStoryboard.style.backgroundImage = "url('img/video/storyboard"+defaultImgNr+".png')";
+}
+// play/pause videos
+videos.forEach(togglePause);
+function togglePause(v){
+    v.addEventListener('click', pauseVideo);
+    function pauseVideo(){
+        if(pause == false){
+            v.play();
+            pause = true;
+            progressLine.style.webkitAnimationPlayState = "running";
+        } else {
+            v.pause();
+            pause = false;
+            progressLine.style.anim
+            progressLine.style.webkitAnimationPlayState = "paused";
+        }
+    }
+}
+
+
