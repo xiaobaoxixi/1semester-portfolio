7b4dcb1e1f6a6fa3081cbf3fc5b07c1bd1a74ec6
Yan Li <Yan@Yans-MacBook-Air.local>
2018-01-03 19:45:12 +0100

    animation: setup all, no moreInfo yet

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,12 @@
 let toggleStageSpan = document.querySelectorAll('.toggleStage span');
 let stages = document.querySelectorAll('.stage');
-
+let worksheet = document.querySelector('img.worksheet');
+let worksheetNr = 1;
+let president = document.querySelector('img.president');
+let drStrangelove = document.querySelector('img.drStrangelove');
+let pilot = document.querySelector('img.pilot');
+let plane = document.querySelector('img.plane');
+// toggle stage for mr.ball
 toggleStageSpan.forEach(clickSpan);
 function clickSpan(s){
     s.addEventListener('click', toggleStage);
@@ -17,4 +23,31 @@ function clickSpan(s){
         let spanClassNr = s.className.replace(/[^0-9\.]/g, '');
         stages[spanClassNr-1].classList.remove('hide');
     }
+}
+// switch worksheets
+worksheet.addEventListener('click', switchWorksheet);
+function switchWorksheet(){
+    if(worksheetNr < 6){
+        worksheetNr++;
+    } else {
+        worksheetNr = 1;
+    }
+    worksheet.src = "img/animation/worksheet" + worksheetNr + ".png";
+}
+// open games
+president.addEventListener('click', openPresident);
+function openPresident(){
+    window.open('http://onestepfurther.science/kea/02-animation/strangelove/conversation.html', '_blank');
+}
+plane.addEventListener('click', openPlane);
+function openPlane(){
+    window.open('http://onestepfurther.science/kea/02-animation/strangelove/planes.html', '_blank');
+}
+pilot.addEventListener('click', openPilot);
+function openPilot(){
+    window.open('http://onestepfurther.science/kea/02-animation/strangelove/pilot1.html', '_blank');
+}
+drStrangelove.addEventListener('click', openDrStrangelove);
+function openDrStrangelove(){
+    window.open('http://onestepfurther.science/kea/02-animation/strangelove/intro.html', '_blank');
 }
