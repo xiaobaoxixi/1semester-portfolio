c4f72baf3b78f1039f450376b0e4d2522847f7d3
Yan Li <Yan@Yans-MacBook-Air.local>
2018-01-05 11:11:10 +0100

    web: opArt - mobile version finished

--- js/web.js
+++ js/web.js
@@ -1,11 +1,15 @@
 let redesign = document.querySelector('.redesign');
+let opArt = document.querySelector('.opArt');
 let toggleToRedesign = document.querySelector('.onOpArt');
+let toggleToRedesign2 = document.querySelector('.onOpArt2');
 let toggleToOpArt = document.querySelector('.onRedesign');
+let toggleToOpArt2 = document.querySelector('.onRedesign2');
 let toFrontpage = document.querySelector('p.frontpage');
 let toContent = document.querySelector('p.content');
 let toNavigation = document.querySelector('p.navigation');
 let device = document.querySelector('.device');
 let computerBase = document.querySelector('.computerBase');
+// switch screen sizes
 function toPadV(){
     device.classList.remove('padH');
     device.classList.remove('computer');
@@ -30,14 +34,19 @@ function toPhone(){
     device.classList.remove('padV');
     computerBase.style.display = "none";
 }
+// switch screen sizes -END
 // toggle between 2 main parts
 toggleToRedesign.addEventListener('click', toRedesign);
+toggleToRedesign2.addEventListener('click', toRedesign);
 function toRedesign(){
     redesign.classList.add('show');
+    opArt.classList.add('hide');
 }
 toggleToOpArt.addEventListener('click', toOpArt);
+toggleToOpArt2.addEventListener('click', toOpArt);
 function toOpArt(){
     redesign.classList.remove('show');
+    opArt.classList.remove('hide');
 }
 // default start with frontpage
 displayFrontPage();
