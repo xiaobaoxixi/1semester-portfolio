78189f97c346ab1230f312b83ab3b90351e5bf7f
Yan Li <Yan@Yans-MacBook-Air.local>
2018-01-06 13:48:27 +0100

    UX: done

--- js/ux.js
+++ js/ux.js
@@ -2,10 +2,7 @@ let moreIcon = document.querySelectorAll('img.more');
 let showInfoSHButton = document.querySelector('img.infoSHIcon');
 let infoSH = document.querySelector('div.infoSH');
 let wdil = document.querySelector('.wdil');
-let slideFat = document.querySelector('.slideFat');
 let aboutLogoDesignWrapper = document.querySelector('.aboutLogoDesignWrapper');
-let aPdf = document.querySelector('a.iframe');
-let iframe = document.querySelector('iframe');
 // toggle info
 showInfoSHButton.addEventListener('click', toggleInfoSH);
 function toggleInfoSH(){
@@ -28,20 +25,4 @@ function clickMoreIcon(m){
             allP[i].classList.toggle('expand');
         }
     }
-}
-// toggle what did i learn (wdil)
-wdil.addEventListener('click', toggleWDIL);
-slideFat.addEventListener('click', toggleWDIL);
-function toggleWDIL(){
-    aboutLogoDesignWrapper.classList.toggle('slideOut');
-    if(slideFat.src.indexOf('Out')>-1){
-        slideFat.src = "img/ux/slideIn-fat.png";
-    } else {
-        slideFat.src = "img/ux/slideOut-fat.png";
-    }
-}
-// open iframe
-aPdf.addEventListener('click', openIframe);
-function openIframe(){
-    iframe.style.height = "600px";
 }
